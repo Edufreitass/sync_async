3b496ef74e4297f9bb727e8cdc1a1c67ddce3261
Eduardo Freitas <eduardoflorencio96@gmail.com>
2023-07-03 23:28:12 -0300

    feat(API): programção assíncrona

--- bin/sync_async.dart
+++ bin/sync_async.dart
@@ -1,5 +1,7 @@
-import 'package:sync_async/sync_async.dart' as sync_async;
-
-void main(List<String> arguments) {
-  print('Hello world: ${sync_async.calculate()}!');
+void main() {
+  var future = Future.delayed(Duration(seconds: 2));
+  future.then((value) => print('novo 4'));
+  print('1');
+  print('2');
+  print('3');
 }
